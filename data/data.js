f8756f5f0f8a7e57b85676711fb19e756a8a429a
antonbeukes <antonbeukes@live.com>
2017-01-25 13:42:14 +0200

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,267 @@
+var personData = {
+    "_id": "5800744a885b5b29afe8d30f",
+    "index": 0,
+    "guid": "56a7cf9d-67f8-4072-9bec-c7a3db8cf998",
+    "isActive": false,
+    "balance": "$1,575.60",
+    "picture": "http://placehold.it/120x120",
+    "age": 20,
+    "eyeColor": "brown",
+    "name": "Booker Anthony",
+    "gender": "male",
+    "company": "QUINTITY",
+    "email": "bookeranthony@quintity.com",
+    "phone": "+1 (963) 593-3024",
+    "address": "982 Commercial Street, Tedrow, Florida, 1252",
+    "about": "Quis commodo exercitation qui et non elit qui magna non. Minim enim anim fugiat fugiat velit sunt amet cillum. Mollit et ad reprehenderit magna ea sit mollit veniam enim qui irure quis in labore.\r\n",
+    "registered": "2016-02-27T08:13:20 -02:00",
+    "latitude": -71.704333,
+    "longitude": 177.511677,
+    "tags": [
+        "nisi",
+        "commodo",
+        "magna",
+        "veniam",
+        "esse",
+        "voluptate",
+        "ea"
+    ],
+    "friends": [{
+        "id": 0,
+        "name": "Frankie Burks",
+        "items": ["item1", "item2", "item3"]
+    }, {
+        "id": 1,
+        "name": "Sherri Matthews",
+        "items": ["item4", "item5", "item6"]
+    }, {
+        "id": 2,
+        "name": "Elise Ross",
+        "items": ["item7", "item8", "item9"]
+    }],
+    "greeting": "Hello, Booker Anthony! You have 3 unread messages.",
+    "favoriteFruit": "banana"
+};
+
+var treeData =[
+  {
+    "id": 0,
+    "node": "Zaya",
+    "children": [
+      {
+        "id": "5809c8ce20e5aa2e2e85e310",
+        "node": "Delphide",
+        "children": [
+          {
+            "id": "5809c8ceb3ebd1cb779b23a1",
+            "node": "Kaitlin Snyder"
+          },
+          {
+            "id": "5809c8cef07c5bdf1883bcf1",
+            "node": "Marian Obrien"
+          },
+          {
+            "id": "5809c8ce71228e5847909629",
+            "node": "Beach Franklin"
+          },
+          {
+            "id": "5809c8ce1f545d604dc7918e",
+            "node": "Vivian Olson"
+          },
+          {
+            "id": "5809c8cef53eb06eb2814188",
+            "node": "Eugenia Hunter"
+          },
+          {
+            "id": "5809c8ced63306b7a66e42a5",
+            "node": "Rodriquez Hines"
+          },
+          {
+            "id": "5809c8ce88e8855f52cffb89",
+            "node": "Florence Terrell"
+          },
+          {
+            "id": "5809c8ce69d09d817ce3384e",
+            "node": "Holt Mckenzie"
+          }
+        ]
+      },
+      {
+        "id": "5809c8ce6d06c382d06a665d",
+        "node": "Qaboos",
+        "children": [
+          {
+            "id": "5809c8ce233f16304355bfae",
+            "node": "Moran Oneill"
+          },
+          {
+            "id": "5809c8ce4d4672a6a0f96319",
+            "node": "Kirsten Alford"
+          },
+          {
+            "id": "5809c8ce8870785f3b5e4d35",
+            "node": "Morrison Knox"
+          }
+        ]
+      },
+      {
+        "id": "5809c8ce8e1a8c47d24684ff",
+        "node": "Zytrex",
+        "children": [
+          {
+            "id": "5809c8ce0bedd8f4d6660d0d",
+            "node": "Henson Joyner"
+          },
+          {
+            "id": "5809c8ceccb16f41cf0e31c7",
+            "node": "Browning Finley"
+          },
+          {
+            "id": "5809c8cef85212d936b48e9f",
+            "node": "Donna Bell"
+          },
+          {
+            "id": "5809c8cecc3ec32ce6e29e85",
+            "node": "Leigh Case"
+          }
+        ]
+      },
+      {
+        "id": "5809c8ce8ced473018a4e406",
+        "node": "Stucco",
+        "children": [
+          {
+            "id": "5809c8ce32a12ae48f9744ab",
+            "node": "Velazquez Kelly"
+          },
+          {
+            "id": "5809c8ce9c64b6c13b1bbc06",
+            "node": "Michael Mcdonald"
+          },
+          {
+            "id": "5809c8ce4aeb09a15948c876",
+            "node": "Kara Stafford"
+          },
+          {
+            "id": "5809c8ce2efc50efb9edd559",
+            "node": "Gay Donaldson"
+          },
+          {
+            "id": "5809c8ce986a0469ef667261",
+            "node": "Parrish Rowland"
+          },
+          {
+            "id": "5809c8ceb3ca4c3e010d4b61",
+            "node": "Ola Nielsen"
+          },
+          {
+            "id": "5809c8ce15d2a4a857a6899c",
+            "node": "Hooper Hickman"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": 1,
+    "node": "Trollery",
+    "children": [
+      {
+        "id": "5809c8ce47ed3629a97e3e84",
+        "node": "Stockpost",
+        "children": [
+          {
+            "id": "5809c8cedcd8298740b6682d",
+            "node": "Jane Boone"
+          }
+        ]
+      },
+      {
+        "id": "5809c8ced601c9c5a58947e3",
+        "node": "Jasper",
+        "children": [
+          {
+            "id": "5809c8ce3e3418134bbbd834",
+            "node": "Irma Fry"
+          },
+          {
+            "id": "5809c8cebc970d2be7c03fae",
+            "node": "Bond Rivers"
+          },
+          {
+            "id": "5809c8ce9e0dcba6f3e74874",
+            "node": "Puckett Vasquez"
+          },
+          {
+            "id": "5809c8ce5ee823bf854dcfb9",
+            "node": "Marci Klein"
+          }
+        ]
+      },
+      {
+        "id": "5809c8ce224884572fe7f64f",
+        "node": "Endicil",
+        "children": [
+          {
+            "id": "5809c8ce0ec9114a3eb87da8",
+            "node": "Lamb Simpson"
+          },
+          {
+            "id": "5809c8ceb38e5cb6d057bf4b",
+            "node": "Victoria Galloway"
+          },
+          {
+            "id": "5809c8cedf6c4ec3ed390a2e",
+            "node": "Lowe Nash"
+          }
+        ]
+      },
+      {
+        "id": "5809c8ced871e9b2904ff86a",
+        "node": "Bleendot",
+        "children": [
+          {
+            "id": "5809c8ce67816cfd66d10639",
+            "node": "Mclaughlin Cotton"
+          },
+          {
+            "id": "5809c8cea5d40278400f59b3",
+            "node": "Gray Talley"
+          },
+          {
+            "id": "5809c8ceaf36ebce9b9258e4",
+            "node": "Owens Harvey"
+          },
+          {
+            "id": "5809c8ce0c3d20db654a7d71",
+            "node": "Edwina Bolton"
+          },
+          {
+            "id": "5809c8cec3813d354527ab95",
+            "node": "Brianna Perez"
+          },
+          {
+            "id": "5809c8ce957d84977296d09d",
+            "node": "Hannah Alvarez"
+          },
+          {
+            "id": "5809c8cec6b83ec508eaf132",
+            "node": "Sykes Woodard"
+          },
+          {
+            "id": "5809c8ce16f25c2a8d973b61",
+            "node": "Warner Burt"
+          },
+          {
+            "id": "5809c8ce652d2dbd57e4ba56",
+            "node": "Benson Bray"
+          },
+          {
+            "id": "5809c8ce18ff44a810dca5fd",
+            "node": "Florine Boyle"
+          }
+        ]
+      }
+    ]
+  }
+]
---
+++ scripts/Emersion.js
@@ -0,0 +1,107 @@
+function init(object, element, templateId) {
+    var templatePath = "templates/" + templateId + ".emr";
+    var templateHTML;
+    ajax.get(templatePath, '', function(data) {templateHTML = replaceTemplateText(object,data);}, false);
+    replaceTemplateText(object,templateHTML)
+    element.innerHTML = templateHTML;
+
+    var emersionElements = element.querySelectorAll('[em]');
+    for (var i = 0; i < emersionElements.length; i++) {
+
+        var funcText = emersionElements[i].getAttribute("em");
+        emersionElements[i].removeAttribute("em")
+
+        var func = praseFunction(funcText, funcText)
+        if (func.command == "forEach") {
+
+            var curObject = object;
+            if (func.objectName != ":model") {
+                curObject = object[func.objectName];
+            }
+            /////////////////////////////////////////////
+            curObject.forEach(function(item, index) {
+                var tempElement = document.createElement("item");
+                init(item, tempElement, func.template)
+
+
+                while (tempElement.children.length > 0) {
+                    var eventElement = tempElement.children[0];
+                     emersionElements[i].appendChild(eventElement)
+                }
+
+            });
+        }
+        else if(func.command == "ifHas") {
+          var curObject = object;
+          if (func.objectName != ":model") {
+              curObject = object[func.objectName];
+          }
+
+          var tempElement = document.createElement("item");
+
+          if (has(curObject,func.name)) {
+            func.template = func.template.substring(0,func.template.indexOf(":"))
+            ;
+          }
+          else {
+            func.template = func.template.substring(func.template.indexOf(":") + 1)
+          }
+            init(curObject, tempElement, func.template);
+          while (tempElement.children.length > 0) {
+              var eventElement = tempElement.children[0];
+              emersionElements[i].appendChild(eventElement)
+          }
+
+          ///////////////////////////////////
+
+        }
+
+    }
+}
+
+function domify(str) {
+    var el = document.createElement('div');
+    el.innerHTML = str;
+    return el;
+}
+
+function replaceTemplateText(object, templateText) {
+  console.log('-------------------------------------------');
+    var rxp = /{([^}]+)}/
+    var curMatch;
+    //find functions
+    while (curMatch = rxp.exec(templateText)) {
+      console.log(curMatch);
+        if (curMatch[1] == ":model") {
+            templateText = templateText.replace(curMatch[0], object.toString())
+        } else {
+            templateText = templateText.replace(curMatch[0], object[curMatch[1]])
+        }
+    };
+      console.log('-------------------------------------------');
+    return templateText;
+}
+
+
+
+
+function praseFunction(text) {
+    var func = {
+        text: "",
+        command: "",
+        objectName: "",
+        name: "",
+        template: ""
+    };
+    func.text = text;
+    func.command = func.text.substring(0, (func.text.indexOf("(")));
+    func.objectName = func.text.substring(func.text.indexOf("(") + 1, func.text.indexOf(")"));
+    var tempString = func.text.substring(func.text.indexOf(" ") + 1);
+    func.name = tempString.substring(0, tempString.indexOf(" "));
+    func.template = tempString.substring(tempString.indexOf(" ") + 1, tempString.length);
+    return func;
+}
+
+function has(object, key) {
+    return object ? hasOwnProperty.call(object, key) : false;
+}
